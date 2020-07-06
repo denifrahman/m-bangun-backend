f833e5cf5a4a4c640ba65829e0523fe74c76fd6f
denifrahman <denifrahman@Macbook-Pro.local>
2020-07-06 23:16:54 +0700

    kontrak fix

--- application/controllers/Kontrak.php
+++ application/controllers/Kontrak.php
@@ -31,7 +31,7 @@ class Kontrak extends CI_Controller
         $produkid = $this->input->get('id', TRUE);
         $data['data_user_bid'] = json_decode(request_api_get(API_GET . '/Bid/getBidByProdukId/' . $produkid, false))->data;
         $data['invoice'] = json_decode(request_api_get(API_GET . '/Invoice/getInvoiceByProdukId/' . $produkid, false))->data;
-        // var_dump($data['invoice']);
+        // var_dump($data['data_user_bid']);
         // die;
         if ($data['data_user_bid'] != null) {
             $this->load->view('kontrak/add_kontrak', $data);
